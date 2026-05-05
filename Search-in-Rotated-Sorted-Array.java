249d23975e801779fff06566a783ffc00581ee38
aniltejavath32-anil <aniltejavath32@gmail.com>
2026-05-05 11:56:20 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 45.76%] - LeetPush

---
+++ Search-in-Rotated-Sorted-Array.java
@@ -0,0 +1,31 @@
+1class Solution {
+2    public int search(int[] nums, int target) {
+3        int left = 0;
+4        int right = nums.length - 1;
+5
+6        while (left <= right) {
+7            int mid = left + (right - left) / 2;
+8
+9            if (nums[mid] == target) {
+10                return mid;
+11            }
+12
+13            if (nums[left] <= nums[mid]) {
+14                if (target >= nums[left] && target < nums[mid]) {
+15                    right = mid - 1;
+16                } else {
+17                    left = mid + 1;
+18                }
+19            } else {
+20                if (target > nums[mid] && target <= nums[right]) {
+21                    left = mid + 1;
+22                } else {
+23                    right = mid - 1;
+24                }
+25            }
+26        }
+27
+28        return -1;
+29    }
+30}
+31
